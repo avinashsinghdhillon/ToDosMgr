3816edb04eacbc386ed9737be18037940e3181c0
Avinash Singh <avinash.dhillon@gmail.com>
2025-02-12 21:17:45 -0500

    Add project files.

---
+++ ToDosMgr/Data/ToDosMgrContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using ToDosMgr;
+
+namespace ToDosMgr.Data
+{
+    public class ToDosMgrContext : DbContext
+    {
+        public ToDosMgrContext (DbContextOptions<ToDosMgrContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<ToDosMgr.ToDoItem> ToDoItem { get; set; } = default!;
+    }
+}
---
+++ ToDosMgr/ToDoItem.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ToDosMgr
+{
+    public class ToDoItem
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public Guid Id { get; set; }
+
+        [Required]
+        [Length(1, 50)]
+        public string? ItemName { get; set; }
+
+        public bool Done { get; set; }
+    }
+}
